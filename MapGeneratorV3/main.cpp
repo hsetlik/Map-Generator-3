ff43697feba5be7da601e241b82c747c8a38efac
hsetlik <haydensetlik@gmail.com>
2020-08-19 12:32:50 -0600

    Initial Commit

---
+++ MapGeneratorV3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MapGeneratorV3
+//
+//  Created by Hayden on 8/19/20.
+//  Copyright © 2020 Hayden Setlik. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
